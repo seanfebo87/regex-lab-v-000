c22212eb28f8564e9da5dfabd1aafe029e0e8f83
Sean Febo <seanfebo@techie.com>
2018-04-10 16:51:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z].*[\.,:;]$/)
     return true
-  elsif text.match(/^[A-Z]\W$/)
+  elsif !text.match(/^[A-Z]\W$/)
   end
 end
 
