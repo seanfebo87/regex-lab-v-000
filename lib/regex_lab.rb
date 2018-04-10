eacec1673420bd7d90a3620bbb76cedd76fdae0c
Sean Febo <seanfebo@techie.com>
2018-04-10 16:50:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z]\W$/)
     return true
   elsif text.match(/^[A-Z]\W$/)
-  
+  end
 end
 
 def valid_phone_number?(phone)
