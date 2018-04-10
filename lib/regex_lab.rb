4c1fac498e640d775a077b703646fedbfacefdc5
Sean Febo <seanfebo@techie.com>
2018-04-10 16:34:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i)
+    return true 
+  elsif !word.match(/\A[aeiou]/i)
+    return false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
+  return text.scan(/\b\w{5}\b/)
 
 end
 
